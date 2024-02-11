908da54ffe3a3da7fff6403cba7e8a19ffa472ae
George Grainger <georgegrainger1008@gmail.com>
2024-02-11 09:14:26 +0000

    Move mongodb into seperate container to overcome issue with host.minikube.internal

--- gateway/server.py
+++ gateway/server.py
@@ -1,20 +1,20 @@
 import gridfs
 import pika
 import json
-from flask import Flask, request
+from flask import Flask, request, send_file
 from flask_pymongo import PyMongo
 from auth import validate
 from auth_service import access
 from storage import util
+from bson.objectid import ObjectId
 
 server = Flask(__name__)
 
-mongo_video = PyMongo(
-    server, uri="mongodb://host.minikube.internal:27017/videos")
-# mongo_mp3 = PyMongo(server, uri="mongodb://host.minikube.internal:27017/mp3s")
+mongo_video = PyMongo(server, uri="mongodb://mongo:27017/videos")
+mongo_mp3 = PyMongo(server, uri="mongodb://mongo:27017/mp3s")
 
 fs_videos = gridfs.GridFS(mongo_video.db)
-# fs_mp3s = gridfs.GridFS(mongo_mp3.db)
+fs_mp3s = gridfs.GridFS(mongo_mp3.db)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
 channel = connection.channel()
